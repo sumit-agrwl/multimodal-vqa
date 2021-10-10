1d8300247212a778a3f8d869bb03cf72af498ffb
Feng Xiang <jason.xi.xiang@gmail.com>
2021-10-10 07:27:19 -0400

    updated files.

--- scripts/Image_ROI_Extraction.py
+++ scripts/Image_ROI_Extraction.py
@@ -63,56 +63,103 @@ if __name__ == "__main__":
             line_count += 1
     
     # TODO open spatial features json file
-    image_features_json_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'spatial','gqa_spatial_info.json')
-    with open(image_features_json_dir) as json_file:
-        image_features_raw_data = json.load(json_file)
+    # image_features_json_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'spatial','gqa_spatial_info.json')
+    # with open(image_features_json_dir) as json_file:
+    #     image_features_raw_data = json.load(json_file)
 
     # TODO open spatial features h5 file
-    image_features_subset_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'spatial')
-    num_image_files = 16
+    # image_features_subset_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'spatial')
+    # num_image_files = 16
 
-    spatial_features_training_dict = {}
+    # spatial_features_training_dict = {}
 
-    for imageID in train_subset_imageID:
-        if imageID not in spatial_features_training_dict.keys():
-            idx = image_features_raw_data[imageID]['idx']
-            file_num = image_features_raw_data[imageID]['file']
-            image_features_filename = "gqa_spatial_" + str(file_num) + ".h5"
-            image_features_filepath = os.path.join(image_features_subset_dir, image_features_filename)
-            h5 = h5py.File(image_features_filepath, 'r')
-            spatial_features_training_dict[imageID] = torch.tensor(h5['features'][idx])
-            h5.close()
-            print("Training: Length of spatial_features_dict: " + str(len(spatial_features_training_dict)))
+    # for imageID in train_subset_imageID:
+    #     if imageID not in spatial_features_training_dict.keys():
+    #         idx = image_features_raw_data[imageID]['idx']
+    #         file_num = image_features_raw_data[imageID]['file']
+    #         image_features_filename = "gqa_spatial_" + str(file_num) + ".h5"
+    #         image_features_filepath = os.path.join(image_features_subset_dir, image_features_filename)
+    #         h5 = h5py.File(image_features_filepath, 'r')
+    #         spatial_features_training_dict[imageID] = torch.tensor(h5['features'][idx])
+    #         h5.close()
+    #         print("Training: Length of spatial_features_dict: " + str(len(spatial_features_training_dict)))
 
     
 
-    image_embedding_train_resnet101_title = "ImageEmbedding_Resnet101_Train.pkl"
-    with open(image_embedding_train_resnet101_title, "wb") as outputfile:
-        pickle.dump(spatial_features_training_dict, outputfile)  
-    del spatial_features_training_dict
+    # image_embedding_train_resnet101_title = "ImageEmbedding_Resnet101_Train.pkl"
+    # with open(image_embedding_train_resnet101_title, "wb") as outputfile:
+    #     pickle.dump(spatial_features_training_dict, outputfile)  
+    # del spatial_features_training_dict
 
-    spatial_features_val_dict = {}
+    # spatial_features_val_dict = {}
 
-    for imageID in val_subset_imageID:
-        if imageID not in spatial_features_val_dict.keys():
-            idx = image_features_raw_data[imageID]['idx']
-            file_num = image_features_raw_data[imageID]['file']
-            image_features_filename = "gqa_spatial_" + str(file_num) + ".h5"
-            image_features_filepath = os.path.join(image_features_subset_dir, image_features_filename)
-            h5 = h5py.File(image_features_filepath, 'r')
-            spatial_features_val_dict[imageID] = torch.tensor(h5['features'][idx])
-            h5.close()
-            print()    
-            print("Validation: Length of spatial_features_dict: " + str(len(spatial_features_val_dict)))
+    # for imageID in val_subset_imageID:
+    #     if imageID not in spatial_features_val_dict.keys():
+    #         idx = image_features_raw_data[imageID]['idx']
+    #         file_num = image_features_raw_data[imageID]['file']
+    #         image_features_filename = "gqa_spatial_" + str(file_num) + ".h5"
+    #         image_features_filepath = os.path.join(image_features_subset_dir, image_features_filename)
+    #         h5 = h5py.File(image_features_filepath, 'r')
+    #         spatial_features_val_dict[imageID] = torch.tensor(h5['features'][idx])
+    #         h5.close()
+    #         print()    
+    #         print("Validation: Length of spatial_features_dict: " + str(len(spatial_features_val_dict)))
 
 
-    image_embedding_val_resnet101_title = "ImageEmbedding_Resnet101_Val.pkl"
-    with open(image_embedding_val_resnet101_title, "wb") as outputfile:
-        pickle.dump(spatial_features_val_dict, outputfile)  
+    # image_embedding_val_resnet101_title = "ImageEmbedding_Resnet101_Val.pkl"
+    # with open(image_embedding_val_resnet101_title, "wb") as outputfile:
+    #     pickle.dump(spatial_features_val_dict, outputfile)  
 
-    del spatial_features_val_dict
+    # del spatial_features_val_dict
+
+    objects_features_json_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'objects','gqa_objects_info.json')
+    with open(objects_features_json_dir) as json_file:
+        objects_features_raw_data = json.load(json_file)
 
 
     # TODO store embeddings that are tied to images to .pkl file
+    objects_features_subset_dir = os.path.join(os.getcwd(), 'gqa', 'allimages', 'objects')
+    num_image_files = 16
+
+    objects_features_training_dict = {}
+
+    for imageID in train_subset_imageID:
+        if imageID not in objects_features_training_dict.keys():
+            idx = objects_features_raw_data[imageID]['idx']
+            file_num = objects_features_raw_data[imageID]['file']
+            objects_features_filename = "gqa_objects_" + str(file_num) + ".h5"
+            objects_features_filepath = os.path.join(objects_features_subset_dir, objects_features_filename)
+            h5 = h5py.File(objects_features_filepath, 'r')
+            objects_features_training_dict[imageID] = torch.tensor(h5['features'][idx])
+            h5.close()
+            print("Training: Length of objects_features_dict: " + str(len(objects_features_training_dict)))
+
+
+
+    objects_embedding_train_resnet101_title = "ImageEmbedding_Resnet101_Train.pkl"
+    with open(objects_embedding_train_resnet101_title, "wb") as outputfile:
+        pickle.dump(objects_features_training_dict, outputfile)  
+    del objects_features_training_dict
+
+    objects_features_val_dict = {}
+
+    for imageID in val_subset_imageID:
+        if imageID not in objects_features_val_dict.keys():
+            idx = objects_features_raw_data[imageID]['idx']
+            file_num = objects_features_raw_data[imageID]['file']
+            objects_features_filename = "gqa_objects_" + str(file_num) + ".h5"
+            objects_features_filepath = os.path.join(objects_features_subset_dir, objects_features_filename)
+            h5 = h5py.File(objects_features_filepath, 'r')
+            objects_features_val_dict[imageID] = torch.tensor(h5['features'][idx])
+            h5.close()
+            print()    
+            print("Validation: Length of objects_features_dict: " + str(len(objects_features_val_dict)))
+
+
+    objects_embedding_val_resnet101_title = "ImageEmbedding_Resnet101_Val.pkl"
+    with open(objects_embedding_val_resnet101_title, "wb") as outputfile:
+        pickle.dump(objects_features_val_dict, outputfile)  
+
+    del objects_features_val_dict
 
     print('Done')
